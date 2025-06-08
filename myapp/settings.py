9d15246ba7bdeabbe025dcbc13729b44cd2b729f
AT-KB <kubo.k3c11@gmail.com>
2025-06-08 21:38:18 +0900

    Document ALLOWED_HOSTS env var

--- myapp/settings.py
+++ myapp/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
+# Allowed hosts configured via environment variable
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
 
