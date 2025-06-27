bdcbae3d47760103876f3980e3a6bd39e3fec74d
AT-KB <kubo.k3c11@gmail.com>
2025-06-27 20:02:25 +0900

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,10 @@
+# myapp/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from core.views import health_check
 
 urlpatterns = [
-    # ヘルスチェック専用エンドポイント
     path("health/", health_check, name="health_check"),
-
-    # Django管理画面
     path("admin/", admin.site.urls),
-
-    # その他のアプリケーションURLはcore.urlsに含める
     path("", include("core.urls")),
 ]
