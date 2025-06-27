035448f260d2663444e2ff2aef9091819d31e104
AT-KB <kubo.k3c11@gmail.com>
2025-06-27 20:02:46 +0900

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
+# core/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ルートパス '' にメインの分析ビューを割り当て
     path('', views.main_analysis_view, name='main_analysis'),
 ]
